81fe9e3413d635b82e8e32502073d6c51eb51d51
ANIL KUMAR <141898587+mahur7830@users.noreply.github.com>
2024-05-15 16:01:20 +0000

    done

---
+++ project1.Color1/color.js
@@ -0,0 +1,32 @@
+//-----------Generate a random color--------//
+const randomColor = function(){
+    const hexCode = '0123456789ABCDEF';
+    let color = '#';
+    for(let i=0; i<6; i++)
+        {
+            color += hexCode[Math.floor(Math.random()*16)];
+            }
+            console.log(color);
+        return color;
+};
+
+let intervalId;
+
+const startChangingColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor,1000)
+    }
+       function changeBgColor(){
+        document.body.style.backgroundColor=randomColor();
+          }
+};
+
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId=null;
+}
+
+
+document.querySelector('#start').addEventListener('click',startChangingColor)
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
